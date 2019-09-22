372279458ceeb1c187901e15694680ab953cbf1c
Jersey Musselman <jm555115@ohio.edu>
2019-09-22 15:17:18 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jm555115_MIS4200_1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jm555115_MIS4200_1.Startup))]
+namespace jm555115_MIS4200_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
